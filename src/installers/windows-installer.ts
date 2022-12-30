fb127309b121e0677562177d36c57b7c12196e9c
Thomas Bailly <3940727+Authfix@users.noreply.github.com>
2022-12-30 17:25:50 +0100

    feat(installer): add windows

--- src/installers/windows-installer.ts
+++ src/installers/windows-installer.ts
@@ -1,4 +1,7 @@
 import * as os from 'os';
+import * as child_process from 'child_process';
+import * as fs from 'fs';
+import request from 'request';
 
 import { IPowershellInstaller } from './installer';
 
@@ -7,7 +10,27 @@ export class WindowsPowershellInstaller implements IPowershellInstaller {
     return os.platform() === 'win32';
   }
 
-  install(): Promise<unknown> {
+  public async install(): Promise<unknown> {
+
+    try {
+      child_process.execSync('where pwsh');
+      return;
+    } catch (error) {
+      console.log('bim')
+    }
+
+    const keyFilePath = fs.createWriteStream('PowerShell-7.3.0-win-x64.msi');
+
+    console.log('bam')
+    await new Promise((resolve,) => {
+      request(
+        `https://github.com/PowerShell/PowerShell/releases/download/v7.3.0/PowerShell-7.3.0-win-x64.msi`, { followRedirect: true }).pipe(keyFilePath).on('close', () => resolve(null));
+    });
+
+    console.log('outch')
+
+    child_process.execSync('msiexec /i "PowerShell-7.3.0-win-x64.msi" /qn')
+
     return Promise.resolve();
   }
 }
